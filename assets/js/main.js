5e81389d71aca4d398c7bd79d476693e0505f117
AGuerrav <aaguerravaldes@gmail.com>
2018-02-06 21:41:28 -0300

    encuentra mi posicion clickeando el boton

--- assets/js/main.js
+++ assets/js/main.js
@@ -24,32 +24,44 @@ function funExito(respuesta) {
 }
 */
 function initMap() {
-
-};
-var map = document.getElementById('map');
-navigator.geolocation.getCurrentPosition(funExito, funError);
-
-function funError() {
-  alert('tenemos un problema para encontrar tu ubicación');
-}
-
-function funExito(respuesta) {
-  // obtengo latitud y longitud de posicion actual
-  var lat = respuesta.coords.latitude;
-  var lon = respuesta.coords.longitude;
-  // le doy mis  parametros a google ( creo objeto google latitud longitud)
-  var gLatLon = new google.maps.LatLng(lat, lon);
-  // creo el mapa de google con la configuracion  que yo quiera para que se renderice
-  var objConfig = {
-    zoom: 17,
-    center: gLatLon
+  var lab = {lat: -12.1191427,
+    lng: -77.0349046};
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 18,
+    center: lab
+  });
+  var markLab = new google.maps.Marker({
+    position: lab,
+    map: map
+  });
+  var map = document.getElementById('map');
+  document.getElementById('findMe').addEventListener('click', findMe);
+  function findMe() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(funExito, funError);
+    }
+  }
+  function funError() {
+    alert('tenemos un problema para encontrar tu ubicación');
+  }
+  function funExito(respuesta) {
+    // obtengo latitud y longitud de posicion actual
+    var lat = respuesta.coords.latitude;
+    var lon = respuesta.coords.longitude;
+    // le doy mis  parametros a google ( creo objeto google latitud longitud)
+    var gLatLon = new google.maps.LatLng(lat, lon);
+    // creo el mapa de google con la configuracion  que yo quiera para que se renderice
+    var objConfig = {
+      zoom: 17,
+      center: gLatLon
+    };
+    var gMapa = new google.maps.Map(map, objConfig);
+    // creo el marcador
+    var objConfigMarker = {
+      position: gLatLon,
+      map: gMapa,
+      title: 'Usted está aquí'
+    };
+    var gMarker = new google.maps.Marker(objConfigMarker);
   };
-  var gMapa = new google.maps.Map(map, objConfig);
-  // creo el marcador
-  var objConfigMarker = {
-    position: gLatLon,
-    map: gMapa,
-    title: 'Usted está aquí'
-  };
-  var gMarker = new google.maps.Marker(objConfigMarker);
 };

98ef71fccdf219e4ce1cb1caa24f784be349fbd3
AGuerrav <aaguerravaldes@gmail.com>
2018-02-06 21:10:47 -0300

    encuentra mi posicion

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,36 +1,55 @@
+/* mostrarObjeto(navigator.geolocation.getCurrentPosition);
+// recorrer el objeto (navigator es un objeto inherente a js)
+function mostrarObjeto(obj) {
+  for (var prop in obj) {
+    console.log(prop + ': ' + obj [prop]);
+  }
+};
+
+navigator.geolocation.getCurrentPosition(funExito, funError);
+var map = document.getElementById('map');
+
+function funError() {
+  map.innerHTML = 'tenemos un problema para encontrar tu ubicación';
+}
+// funcion que se desencadenara cuando el usuario acepte verificar su ubicacion
+// parametro respuesta es obtenida atuomaticamente
+function funExito(respuesta) {
+  // mostrarObjeto(respuesta.coords);
+  // map.innerHTML = 'podemos encontrar tu ubicación';
+
+  var lat = respuesta.coords.latitude;
+  var lon = respuesta.coords.longitude;
+  map.innerHTML = lat + ',' + lon ;
+}
+*/
 function initMap() {
-  var lab = {lat: -12.1191427,
-    lng: -77.0349046};
-  var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 18,
-    center: miUbicacion
-  });
-  var markLab = new google.maps.Marker({
-    position: lab,
-    map: map
-  });
+
+};
+var map = document.getElementById('map');
+navigator.geolocation.getCurrentPosition(funExito, funError);
+
+function funError() {
+  alert('tenemos un problema para encontrar tu ubicación');
 }
 
-function findMe() {
-  // verificamos si el navegador soporta la geolocalización
-//   var output = document.getElementById('map');
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(funExito, funError);
-  }
-  // primera funcion que utilizaremos como parametro para getCurrentPosition
-  var latitud, longitud;
-  var funExito = function(position) {
-    latitud = position.coords.latitude;
-    longitud = position.coords.longitude;
+function funExito(respuesta) {
+  // obtengo latitud y longitud de posicion actual
+  var lat = respuesta.coords.latitude;
+  var lon = respuesta.coords.longitude;
+  // le doy mis  parametros a google ( creo objeto google latitud longitud)
+  var gLatLon = new google.maps.LatLng(lat, lon);
+  // creo el mapa de google con la configuracion  que yo quiera para que se renderice
+  var objConfig = {
+    zoom: 17,
+    center: gLatLon
   };
-  var miUbicacion = new google.maps.Marker({
-    position: {lat: latitud,
-      lng: longitud},
-    map: map
-  });
-
-  // segundo parametro para getCurrentPosition
-  var funError = function(error) {
-    alert('tenemos un problema para encontrar tu ubicación');
-  }
-}
+  var gMapa = new google.maps.Map(map, objConfig);
+  // creo el marcador
+  var objConfigMarker = {
+    position: gLatLon,
+    map: gMapa,
+    title: 'Usted está aquí'
+  };
+  var gMarker = new google.maps.Marker(objConfigMarker);
+};
